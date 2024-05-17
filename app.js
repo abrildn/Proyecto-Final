b63db4f571f76d456d699140894a232ca7679dd4
Abril <abrudomingueznoya@gmail.com>
2024-05-17 15:15:05 -0300

    added classes pages

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 function changeVideo(videoId) {
-    let iframe = document.getElementById('expansions-videos');
+    let iframe = document.getElementById('change-video');
     iframe.src = 'http://www.youtube.com/embed/' + videoId;
 }
-
