0a588f5bdc312667a844b82db5d2ca286b765f10
Abril <abrudomingueznoya@gmail.com>
2024-05-29 12:32:42 -0300

    añadí formulario

--- app.js
+++ app.js
@@ -2,3 +2,59 @@ function changeVideo(videoId) {
     let iframe = document.getElementById('change-video');
     iframe.src = 'http://www.youtube.com/embed/' + videoId;
 }
+
+function saveInfo() {
+    const nameSent = document.querySelector("#name").value;
+    const ageSent = document.querySelector("#age").value;
+    const classSent = document.querySelector("#class").value;
+    const zoneSent = document.querySelector("#zone").value;
+    const songSent = document.querySelector("#song").value;
+    const infoSent = document.querySelector("#info").value;
+
+    const users = JSON.parse( localStorage.getItem("users") ) || [];
+
+    users.push({
+        name: nameSent,
+        age: ageSent,
+        class: classSent,
+        zone: zoneSent,
+        song: songSent,
+        info: infoSent
+    });
+
+    localStorage.setItem("users", JSON.stringify(users));
+}
+
+function getInfo() {
+    const olUsers = document.querySelector("#info-users");
+    const users = JSON.parse(localStorage.getItem("users")) || [];
+
+    users.forEach(user => {
+        olUsers.innerHTML+=`
+            <li class="user">
+                <ul>
+                    <li>
+                        Name: ${user.name}
+                    </li>
+                    <li>
+                        Age: ${user.age}
+                    </li>
+                    <li>
+                        Prefered Class: ${user.class}
+                    </li>
+                    <li>
+                        Prefered Zone: ${user.zone}
+                    </li>
+                    <li>
+                        Prefered Song: ${user.song}
+                    </li>
+                    <li>
+                        How you find out about Warcraft: ${user.info}
+                    </li>
+                </ul>
+            </li>
+        `;
+    });
+}
+
+getInfo();
